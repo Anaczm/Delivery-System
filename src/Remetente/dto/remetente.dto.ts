b0459b32203baadf04f430e04888440e6156fb60
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-04-17 23:40:05 -0300

    [ feat ] DTO implementation.

---
+++ src/Remetente/dto/remetente.dto.ts
@@ -0,0 +1,10 @@
+export interface RemetenteDto {
+    str_nome: string;
+    str_telefone: string;
+    str_CPF: string;
+    str_estado: string;
+    str_cidade: string;
+    str_bairro: string;
+    str_numero: string;
+    str_CEP: string;
+}
