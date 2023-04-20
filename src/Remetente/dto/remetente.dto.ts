4033c81e70cd219ede9f87acb67f410c4fe36160
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-04-20 00:13:29 -0300

    [ feat ] Validation of "Remetente" fields created.

--- src/Remetente/dto/remetente.dto.ts
+++ src/Remetente/dto/remetente.dto.ts
@@ -1,10 +1,39 @@
-export interface RemetenteDto {
+import { IsNotEmpty, IsString, IsPhoneNumber, IsPostalCode, Validate} from "class-validator";
+import { cpf }from "cpf-cnpj-validator"
+
+export class RemetenteDto {
+    @IsNotEmpty()
+    @IsString()
     str_nome: string;
+
+    @IsNotEmpty()
+    @IsPhoneNumber('BR')
     str_telefone: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Validate((valor) => cpf.isValid(valor), {
+        message: 'CPF inválido',
+    })
     str_CPF: string;
+    
+    @IsNotEmpty()
+    @IsString()
     str_estado: string;
+
+    @IsNotEmpty()
+    @IsString()
     str_cidade: string;
+
+    @IsNotEmpty()
+    @IsString()
     str_bairro: string;
+
+    @IsNotEmpty()
+    @IsString()
     str_numero: string;
+
+    @IsNotEmpty()
+    @IsPostalCode('BR')
     str_CEP: string;
 }
