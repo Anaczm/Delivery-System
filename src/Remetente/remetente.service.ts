ddbbf24032f4ac6b87e1c3099d01f2a4b9561f1c
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-04-28 22:04:59 -0300

    [ feat ] Remetente register created on database.

--- src/Remetente/remetente.service.ts
+++ src/Remetente/remetente.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { RemetenteDto } from "./dto";
 
-@Injectable({})
+@Injectable()
     export class RemetenteService {
         constructor(private prisma: PrismaService) {}
-        cadastro() {
-            return { msg: 'Remetente cadastrado! Just a request, not a database messege :|'}
+        
+        async cadastro(dto: RemetenteDto) {
+
+            const remetente = await this.prisma.remetente.create({
+                data: {
+                    str_nome: dto.str_nome,
+                    str_telefone: dto.str_telefone,
+                    str_CPF: dto.str_CPF,
+                    str_estado: dto.str_estado,
+                    str_cidade: dto.str_cidade,
+                    str_bairro: dto.str_bairro,
+                    str_numero: dto.str_numero,
+                    str_CEP: dto.str_CEP,
+                },
+                select: {
+                    id: true,
+                    id_objeto: true,
+                },
+            });
+
+            return remetente;
         }
     }
