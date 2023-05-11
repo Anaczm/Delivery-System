8afb4a9f10f99e13861c2f9a61fa8ca33e118a0b
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-05-10 22:57:21 -0300

    [ update ]
    name of sender module changed. Object and Recipient modules excluded

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RemetenteModule } from './Remetente/remetente.module';
-import { DestinatarioModule } from './Destinatario/destinatario.module';
-import { ObjetoModule } from './Objeto/objeto.module';
+import { SenderModule } from './Sender/sender.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [RemetenteModule, DestinatarioModule, ObjetoModule, PrismaModule],
+  imports: [SenderModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
