b5952b9b9764c38f0f147c271ef28fcea1cd6275
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-05-12 01:16:36 -0300

    [ build ] error response configured

--- src/Sender/sender.service.ts
+++ src/Sender/sender.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { SenderDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
     export class SenderService {
@@ -8,23 +9,34 @@ import { SenderDto } from "./dto";
         
         async register(dto: SenderDto) {
 
-            const sender = await this.prisma.sender.create({
-                data: {
-                    str_name: dto.str_name,
-                    str_phone: dto.str_phone,
-                    str_CPF: dto.str_CPF,
-                    str_state: dto.str_state,
-                    str_city: dto.str_city,
-                    str_neiborhood: dto.str_neiborhood,
-                    str_number: dto.str_number,
-                    str_CEP: dto.str_CEP,
-                },
-                select: {
-                    id: true,
-                    id_object: true,
-                },
-            });
-
-            return sender;
+            try {
+                const sender = await this.prisma.sender.create({
+                    data: {
+                        str_name: dto.str_name,
+                        str_phone: dto.str_phone,
+                        str_CPF: dto.str_CPF,
+                        str_state: dto.str_state,
+                        str_city: dto.str_city,
+                        str_neiborhood: dto.str_neiborhood,
+                        str_number: dto.str_number,
+                        str_CEP: dto.str_CEP,
+                    },
+                    select: {
+                        id: true,
+                        id_object: true,
+                    },
+                });
+    
+                return sender;
+            } catch (error) {
+                if (error instanceof PrismaClientKnownRequestError) {
+                    if (error.code === 'P2002') {
+                        throw new ForbiddenException(
+                            'Credentials taken',
+                        );
+                    }
+                }
+                throw error;
+            }
         }
     }
