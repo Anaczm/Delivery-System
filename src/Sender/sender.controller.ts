315c11cc76a61b790d111e0cf458510fb6941cdf
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-05-12 01:10:38 -0300

    [ style ] sintax space

--- src/Sender/sender.controller.ts
+++ src/Sender/sender.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Post }   from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SenderService } from './sender.service';
 import { SenderDto } from './dto';
 
 @Controller()
 export class SenderController {
-    constructor( private senderService: SenderService) {}
+    constructor (private senderService: SenderService) {}
 
     @Post('register')
     register(@Body() dto: SenderDto) {
