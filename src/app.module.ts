8ef50f558095b858f9dd8508936c2884a1610926
Ana Carolina <a.carolina.zumarinho@gmail.com>
2023-05-20 00:17:29 -0300

    [ build ] Recipient module added in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SenderModule } from './Sender/sender.module';
+import { RecipientModule } from './Recipient/recipient.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [SenderModule, PrismaModule],
+  imports: [SenderModule, RecipientModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
